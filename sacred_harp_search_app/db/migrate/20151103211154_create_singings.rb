abc2694ceb0745ba72915f80e11b25c85f518554
racheltstevens <racheltstevens@gmail.com>
2015-11-05 16:41:59 -0500

    Adds validation constraints to create_singings migration

--- sacred_harp_search_app/db/migrate/20151103211154_create_singings.rb
+++ sacred_harp_search_app/db/migrate/20151103211154_create_singings.rb
@@ -1,12 +1,14 @@
 class CreateSingings < ActiveRecord::Migration
   def change
     create_table :singings do |t|
-      t.string :name
-      t.string :city
-      t.string :state
-      t.date :date
+      t.string :name, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.start_date :date, null: false
+      t.end_date :date, null: false
 
       t.timestamps null: false
     end
   end
+  add_index :singings, [:name, :city, :start_date], unique: true
 end
