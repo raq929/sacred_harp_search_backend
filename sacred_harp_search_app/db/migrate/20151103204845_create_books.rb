1bfd54ddbabb7d4edc77ba3ecd4d5675a8894472
racheltstevens <racheltstevens@gmail.com>
2015-11-05 16:43:56 -0500

    Adds unique requirement to book names in CreateBooks migration

--- sacred_harp_search_app/db/migrate/20151103204845_create_books.rb
+++ sacred_harp_search_app/db/migrate/20151103204845_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.string :name, null: false
+      t.string :name, null: false, unique: true
 
       t.timestamps null: false
     end
