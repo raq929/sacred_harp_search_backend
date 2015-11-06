b83f4046406ef2ece5e95969349c3fb1511557c7
racheltstevens <racheltstevens@gmail.com>
2015-11-06 14:53:24 -0500

    Addsschema.rb

--- sacred_harp_search_app/db/schema.rb
+++ sacred_harp_search_app/db/schema.rb
@@ -59,6 +59,17 @@ ActiveRecord::Schema.define(version: 20151103212113) do
 
   add_index "songs", ["book_id"], name: "index_songs_on_book_id", using: :btree
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email",           null: false
+    t.string   "token",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
+
   add_foreign_key "calls", "callers"
   add_foreign_key "calls", "singings"
   add_foreign_key "calls", "songs"
