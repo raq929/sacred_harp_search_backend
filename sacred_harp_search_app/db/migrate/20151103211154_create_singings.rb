4601717f0aff4c09658a50d69ab5d0cdc97b1c91
racheltstevens <racheltstevens@gmail.com>
2015-11-08 12:10:29 -0500

    Changes singing migration and model to reflect the data coming in.
    *date is now singular
    *instead of city and state, there is one location field.

--- sacred_harp_search_app/db/migrate/20151103211154_create_singings.rb
+++ sacred_harp_search_app/db/migrate/20151103211154_create_singings.rb
@@ -2,13 +2,11 @@ class CreateSingings < ActiveRecord::Migration
   def change
     create_table :singings do |t|
       t.string :name, null: false
-      t.string :city, null: false
-      t.string :state, null: false
-      t.start_date :date, null: false
-      t.end_date :date, null: false
+      t.string :location, null: false
+      t.string :date, null: false
 
       t.timestamps null: false
     end
   end
-  add_index :singings, [:name, :start_date], unique: true
+  add_index :singings, [:name, :date], unique: true
 end
