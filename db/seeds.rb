def5833f6ed472722b2900bcc63caa6ad460b811
racheltstevens <racheltstevens@gmail.com>
2015-11-12 09:16:44 -0500

    updates seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,14 @@ Caller.destroy_all
 Singing.destroy_all
 User.destroy_all
 
-Book.find_or_create_by(name:"1991 Sacred Harp")
-book = Book.find_by(name: "1991 Sacred Harp")
-p book
+Book.create!(name:"1991 Sacred Harp")
+Book.create!(name:"Shenandoah Harmony")
+
+Song.create!(name:"Fire Alarm", number:"25")
+Song.create!(name:"Chapter X - Singing Excercises", number:"24t")
+Song.create!(name:"The Young Convert", number:"24b")
+
+
 
 CSV.foreach "../data/SongData_Denson_1991.txt", {headers: true, encoding: "MacRoman:UTF-8"} do |row|
    Song.find_or_create_by!(number: row["PageNum"], book_id: book[:id], name: row["Title"], meter_name: row["MeterName"], meter_count: row["MeterCount"], song_text: row["SongText"], composer_first_name: row["Comp1First"], composer_last_name: row["Comp1Last"], composition_date: row["Comp1Date"], poet_first_name: row["Poet1First"], post_last_name: row["Poet1Last"])
@@ -38,7 +43,6 @@ minutes.each do |singing|
            found = Song.find_by(number: match[1])
            song_id = found[:id]
            Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song_id)
-           puts "Call created - valid"
         else
         # matches ambiguous calls
         # returns MatchData or nil
@@ -47,7 +51,6 @@ minutes.each do |singing|
           if match
             song = Song.find_or_create_by!(number: match[1], book_id: Book.find_by(name:"1991 Sacred Harp")[:id], name: "Ambiguous Call")
             Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
-            puts "Call created - ambiguous"
           else
             # matches corrected calls
             # returns MatchData or nil
@@ -56,7 +59,6 @@ minutes.each do |singing|
             match = /\[(\d+[tb]?)\/\/(\d+[tb]?)\]/.match(song)
             if match
               Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: Song.find_by(number: match[2])[:id])
-              puts "Call created - corrected"
             else
               # matches uncorrectable calls
               # returns MatchData or nil
@@ -65,7 +67,6 @@ minutes.each do |singing|
               if match
                 song = Song.find_or_create!(number: match[1], book_id: Book.find_by("1991 Sacred Harp"), name: "Uncorrectable Call")
                 Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
-                puts "Call created - uncorrectable"
               end
             end
           end
