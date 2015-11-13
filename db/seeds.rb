b133557794e40a6d6b62ab241e9f042ecee57c78
racheltstevens <racheltstevens@gmail.com>
2015-11-13 07:15:29 -0500

    Updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,18 @@ Caller.destroy_all
 Singing.destroy_all
 User.destroy_all
 
-Book.create!(name:"1991 Sacred Harp")
-Book.create!(name:"Shenandoah Harmony")
+harp = Book.create!(name:"1991 Sacred Harp")
+shen = Book.create!(name:"Shenandoah Harmony")
 
-Song.create!(name:"Fire Alarm", number:"25")
-Song.create!(name:"Chapter X - Singing Excercises", number:"24t")
-Song.create!(name:"The Young Convert", number:"24b")
+Song.create!(name:"Fire Alarm", number:"25", book: harp.id)
+Song.create!(name:"Chapter X - Singing Excercises", number:"24t", book: harp.id)
+Song.create!(name:"The Young Convert", number:"24b", book: harp.id)
+User.create!(email: "r@r.com", password: "123", admin: true, book: harp.id)
 
 
 
 CSV.parse "https://s3.amazonaws.com/sacredharpsearch/SongData_Denson_1991.txt", {headers: true, encoding: "MacRoman:UTF-8"} do |row|
-   Song.find_or_create_by!(number: row["PageNum"], book_id: book[:id], name: row["Title"], meter_name: row["MeterName"], meter_count: row["MeterCount"], song_text: row["SongText"], composer_first_name: row["Comp1First"], composer_last_name: row["Comp1Last"], composition_date: row["Comp1Date"], poet_first_name: row["Poet1First"], poet_last_name: row["Poet1Last"])
+   Song.find_or_create_by!(number: row["PageNum"], book_id: shen[:id], name: row["Title"], meter_name: row["MeterName"], meter_count: row["MeterCount"], song_text: row["SongText"], composer_first_name: row["Comp1First"], composer_last_name: row["Comp1Last"], composition_date: row["Comp1Date"], poet_first_name: row["Poet1First"], poet_last_name: row["Poet1Last"])
 end
 
 
@@ -49,7 +50,7 @@ minutes.each do |singing|
         # capture 1 is song number
           match = /\{(\d+[tb]?)\}/.match(song)
           if match
-            song = Song.find_or_create_by!(number: match[1], book_id: Book.find_by(name:"1991 Sacred Harp")[:id], name: "Ambiguous Call")
+            song = Song.find_or_create_by!(number: match[1], book_id: harp[:id], name: "Ambiguous Call")
             Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
           else
             # matches corrected calls
@@ -65,7 +66,7 @@ minutes.each do |singing|
               # capture 1 is song number
               match = /<(\d+[tb]?)>/.match(song)
               if match
-                song = Song.find_or_create!(number: match[1], book_id: Book.find_by("1991 Sacred Harp"), name: "Uncorrectable Call")
+                song = Song.find_or_create!(number: match[1], book_id: harp[:id], name: "Uncorrectable Call")
                 Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
               end
             end
