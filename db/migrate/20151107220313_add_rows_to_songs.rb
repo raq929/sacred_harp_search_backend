87dc047ad9eadcee4f011a66d3a8e2895e08d48f
racheltstevens <racheltstevens@gmail.com>
2015-11-13 07:47:52 -0500

    fixes typo in add_rows_to_songs migration

--- db/migrate/20151107220313_add_rows_to_songs.rb
+++ db/migrate/20151107220313_add_rows_to_songs.rb
@@ -8,7 +8,7 @@ class AddRowsToSongs < ActiveRecord::Migration
     add_column :songs, :composer_last_name, :string
     add_column :songs, :composition_date, :string
     add_column :songs, :poet_first_name, :string
-    add_column :songs, :post_last_name, :string
+    add_column :songs, :poet_last_name, :string
 
     add_index :songs, [:name, :title_ordinal, :book_id], unique: true
   end
