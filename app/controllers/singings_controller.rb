820048cc93f58074badeff9a127a4d6a0c5ab041
racheltstevens <racheltstevens@gmail.com>
2015-12-06 11:37:14 -0500

    Fixes create singing for Denson book
    * adds script for parsing song markup
    * WOoooooooooo!

--- app/controllers/singings_controller.rb
+++ app/controllers/singings_controller.rb
@@ -22,17 +22,54 @@ def parse_minutes_shenandoah singing_id, csv
 end
 
 def parse_minutes_denson singing_id, csv
-
+  puts "singing id = ", singing_id
   book_id = Book.find_by(name:"1991 Sacred Harp")[:id]
   minutes = denson_parse_one(csv)
-  # render json: minutes
-  # minutes["Singers"].each do |call|
-  #   caller_id = Caller.find_or_create_by!(name: call["name"])[:id]
-  #   call["songs"].each do |song|
-  #     song_id = Song.find_by(number: song, book_id: book_id)[:id]
-  #     Call.create!(song_id: song_id, caller_id: caller_id, singing_id: singing_id)
-  #   end
-  # end
+  minutes["Singers"].each do |call|
+  def create_call_for_existing_song song_number, caller_id, singing_id
+    found = Song.find_by!(number: song_number)
+    song_id = found[:id]
+    Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song_id)
+  end
+
+  caller_id = Caller.find_or_create_by!(name: call["name"])[:id]
+  call["songs"].each do |song|
+    # matches unambiguous calls
+      # returns MatchData or nil
+      # capture 1 is song number
+      match =  /\[(\d+[tb]?)\]/.match(song)
+      if match 
+         create_call_for_existing_song match[1], caller_id, singing_id
+      else
+      # matches ambiguous calls
+      # returns MatchData or nil
+      # capture 1 is song number
+        match = /\{(\d+[tb]?)\}/.match(song)
+        if match
+          song = Song.find_or_create_by!(number: match[1], book_id: book_id, name: "Ambiguous Call")
+          Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
+        else
+          # matches corrected calls
+          # returns MatchData or nil
+          # capture 1 is original song number
+          # capture 2 is corrected song number
+          match = /\[(\d+[tb]?)\/\/(\d+[tb]?)\]/.match(song)
+          if match
+            create_call_for_existing_song match[2], caller_id, singing_id
+          else
+            # matches uncorrectable calls
+            # returns MatchData or nil
+            # capture 1 is song number
+            match = /<(\d+[tb]?)>/.match(song)
+            if match
+              song = Song.find_or_create!(number: match[1], book_id: book_id, name: "Uncorrectable Call")
+              Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
 def denson_parse_one text
@@ -53,9 +90,9 @@ def denson_parse_one text
   fieldnames    = nil
   recnum      = 0
   rec = nil
-  count = 0
+ 
   CSV.parse(text, {headers: false, :col_sep => "\t"}).each do |row|
-    count +=1
+
     # Read the header row
     begin fieldnames = row; next end unless fieldnames
   
@@ -82,19 +119,21 @@ def denson_parse_one text
 
     minutes     = rec["Minutes"]
 
-    # singers_raw   = minutes.scan(singer_match)
+    singers_raw   = minutes.scan(singer_match)
 
-    # rec['Singers']  = singers_raw.map{|pair|
-    #   name, songs   = pair
-    #   songlist    = songs.scan(song_markup).flatten
-    #   info      = {
-    #   'name'    => name,
-    #   # 'songs_raw' => songs,
-    #   'songs'   => songlist,
-    #   }
-    # }
+    rec['Singers']  = singers_raw.map{|pair|
+      name, songs   = pair
+      songlist    = songs.scan(song_markup).flatten
+      info      = {
+      'name'    => name,
+      # 'songs_raw' => songs,
+      'songs'   => songlist,
+      }
+    }
+
+
   end
-  p "Count = ", count
+  rec
 end
 
 class SingingsController < OpenReadController
