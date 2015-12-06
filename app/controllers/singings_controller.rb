a63785b55c0ece5958c2ceae784dca436d60c329
racheltstevens <racheltstevens@gmail.com>
2015-12-06 12:21:33 -0500

    Moves denson_parse_one and minutes_parser into modules in the lib directory, requires them

--- app/controllers/singings_controller.rb
+++ app/controllers/singings_controller.rb
@@ -1,142 +1,146 @@
-# require "./scripts/minutes_parser.rb"
+require_relative '../../lib/minutes_parser.rb'
+require_relative '../../lib/denson_parse_one.rb'
 
 require 'csv'
 # require './scripts/denson_parse_one.rb'
 
-def parse_minutes_shenandoah singing_id, csv
-    # render json: singing_id
-    calls = Array.new
-    book_id = Book.find_by(name:"Shenandoah Harmony")[:id]
+# def parse_minutes_shenandoah singing_id, csv
+#     # render json: singing_id
+#     calls = Array.new
+#     book_id = Book.find_by(name:"Shenandoah Harmony")[:id]
 
-    CSV.parse(csv, {headers: true}) do |call|
+#     CSV.parse(csv, {headers: true}) do |call|
 
-        song_id = Song.find_or_create_by!(number: call["Page"], name: call["Song Title"], book_id: book_id)[:id]
-        caller_id = Caller.find_or_create_by!(name: call["Name(s)"])[:id]
+#         song_id = Song.find_or_create_by!(number: call["Page"], name: call["Song Title"], book_id: book_id)[:id]
+#         caller_id = Caller.find_or_create_by!(name: call["Name(s)"])[:id]
 
-        new_call = Call.create!(song_id: song_id, caller_id: caller_id, singing_id: singing_id)
-        calls.push(new_call)
+#         new_call = Call.create!(song_id: song_id, caller_id: caller_id, singing_id: singing_id)
+#         calls.push(new_call)
     
         
-    end
-    render json: {calls: calls}
-end
+#     end
+#     render json: {calls: calls}
+# end
 
-def parse_minutes_denson singing_id, csv
-  puts "singing id = ", singing_id
-  book_id = Book.find_by(name:"1991 Sacred Harp")[:id]
-  minutes = denson_parse_one(csv)
-  minutes["Singers"].each do |call|
-  def create_call_for_existing_song song_number, caller_id, singing_id
-    found = Song.find_by!(number: song_number)
-    song_id = found[:id]
-    Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song_id)
-  end
+# def parse_minutes_denson singing_id, csv
+#   puts "singing id = ", singing_id
+#   book_id = Book.find_by(name:"1991 Sacred Harp")[:id]
+#   minutes = denson_parse_one(csv)
+#   minutes["Singers"].each do |call|
+#   def create_call_for_existing_song song_number, caller_id, singing_id
+#     found = Song.find_by!(number: song_number)
+#     song_id = found[:id]
+#     Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song_id)
+#   end
 
-  caller_id = Caller.find_or_create_by!(name: call["name"])[:id]
-  call["songs"].each do |song|
-    # matches unambiguous calls
-      # returns MatchData or nil
-      # capture 1 is song number
-      match =  /\[(\d+[tb]?)\]/.match(song)
-      if match 
-         create_call_for_existing_song match[1], caller_id, singing_id
-      else
-      # matches ambiguous calls
-      # returns MatchData or nil
-      # capture 1 is song number
-        match = /\{(\d+[tb]?)\}/.match(song)
-        if match
-          song = Song.find_or_create_by!(number: match[1], book_id: book_id, name: "Ambiguous Call")
-          Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
-        else
-          # matches corrected calls
-          # returns MatchData or nil
-          # capture 1 is original song number
-          # capture 2 is corrected song number
-          match = /\[(\d+[tb]?)\/\/(\d+[tb]?)\]/.match(song)
-          if match
-            create_call_for_existing_song match[2], caller_id, singing_id
-          else
-            # matches uncorrectable calls
-            # returns MatchData or nil
-            # capture 1 is song number
-            match = /<(\d+[tb]?)>/.match(song)
-            if match
-              song = Song.find_or_create!(number: match[1], book_id: book_id, name: "Uncorrectable Call")
-              Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
-            end
-          end
-        end
-      end
-    end
-  end
-end
+#   caller_id = Caller.find_or_create_by!(name: call["name"])[:id]
+#   call["songs"].each do |song|
+#     # matches unambiguous calls
+#       # returns MatchData or nil
+#       # capture 1 is song number
+#       match =  /\[(\d+[tb]?)\]/.match(song)
+#       if match 
+#          create_call_for_existing_song match[1], caller_id, singing_id
+#       else
+#       # matches ambiguous calls
+#       # returns MatchData or nil
+#       # capture 1 is song number
+#         match = /\{(\d+[tb]?)\}/.match(song)
+#         if match
+#           song = Song.find_or_create_by!(number: match[1], book_id: book_id, name: "Ambiguous Call")
+#           Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
+#         else
+#           # matches corrected calls
+#           # returns MatchData or nil
+#           # capture 1 is original song number
+#           # capture 2 is corrected song number
+#           match = /\[(\d+[tb]?)\/\/(\d+[tb]?)\]/.match(song)
+#           if match
+#             create_call_for_existing_song match[2], caller_id, singing_id
+#           else
+#             # matches uncorrectable calls
+#             # returns MatchData or nil
+#             # capture 1 is song number
+#             match = /<(\d+[tb]?)>/.match(song)
+#             if match
+#               song = Song.find_or_create!(number: match[1], book_id: book_id, name: "Uncorrectable Call")
+#               Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
+#             end
+#           end
+#         end
+#       end
+#     end
+#   end
+# end
 
-def denson_parse_one text
+# def denson_parse_one text
   
-  # Definition of format for parsing people's names and songs:
-  songnum     = %r{\d{2,3}[tb]?}                # 24 or 306 or 306t or 95b
-  song_markup   = %r{(?:\[#{songnum}\]) |           # [306t]
-               (?:\{#{songnum}\}) |           # {306}
-               (?:\[#{songnum}//#{songnum}\])       # [306//306t]
-              }x
-  song_sequence   = %r{#{song_markup}(?:(?:, ?#{song_markup})*)}  # [28], {29} ... etc.
-  name_part     = %r{(?:[[:upper:]][[:alpha:]\.\-\']*)}     # B or B. or B.G. or Bill or BILL but not 'bill'
-  person_name   = %r{#{name_part}(?:(?: #{name_part})*)}    # One or more name_part separated by spaces
-  singer_match    = %r{(#{person_name}) (#{song_sequence})}   # A name followed by a list of songs
+#   # Definition of format for parsing people's names and songs:
+#   songnum     = %r{\d{2,3}[tb]?}                # 24 or 306 or 306t or 95b
+#   song_markup   = %r{(?:\[#{songnum}\]) |           # [306t]
+#                (?:\{#{songnum}\}) |           # {306}
+#                (?:\[#{songnum}//#{songnum}\])       # [306//306t]
+#               }x
+#   song_sequence   = %r{#{song_markup}(?:(?:, ?#{song_markup})*)}  # [28], {29} ... etc.
+#   name_part     = %r{(?:[[:upper:]][[:alpha:]\.\-\']*)}     # B or B. or B.G. or Bill or BILL but not 'bill'
+#   person_name   = %r{#{name_part}(?:(?: #{name_part})*)}    # One or more name_part separated by spaces
+#   singer_match    = %r{(#{person_name}) (#{song_sequence})}   # A name followed by a list of songs
 
-  # Parse...
+#   # Parse...
 
-  fieldnames    = nil
-  recnum      = 0
-  rec = nil
+#   fieldnames    = nil
+#   recnum      = 0
+#   rec = nil
  
-  CSV.parse(text, {headers: false, :col_sep => "\t"}).each do |row|
+#   CSV.parse(text, {headers: false, :col_sep => "\t"}).each do |row|
 
-    # Read the header row
-    begin fieldnames = row; next end unless fieldnames
+#     # Read the header row
+#     begin fieldnames = row; next end unless fieldnames
   
-    # Read regular rows, decoding embedded newlines, removing " "
-    # around fields, and parsing values that appear to be floats or
-    # integers.
+#     # Read regular rows, decoding embedded newlines, removing " "
+#     # around fields, and parsing values that appear to be floats or
+#     # integers.
 
-    # Not handling encoding of " as "" inside field values because
-    # that does not occur in the input data, although it is part of
-    # the spec for "Merge" format.
+#     # Not handling encoding of " as "" inside field values because
+#     # that does not occur in the input data, although it is part of
+#     # the spec for "Merge" format.
 
-    rec       = Hash[fieldnames.zip(row.map{|i|
-                        i.gsub!(/\x0B/, "\n")         # Merge format newlines embedded as ASCII 11 (Control-K aka \x0B).
-                        i.gsub!(/^\"?(.*)\"$/m, "\\1")    # Trim leading and trailing quotes.
-                        i = ( (i.is_a? Integer )  ? Integer(i) : # Convert to int or float if possible.
-                           ((i.is_a? Float) ?   Float(i) : i))
-                        })]
+#     rec       = Hash[fieldnames.zip(row.map{|i|
+#                         i.gsub!(/\x0B/, "\n")         # Merge format newlines embedded as ASCII 11 (Control-K aka \x0B).
+#                         i.gsub!(/^\"?(.*)\"$/m, "\\1")    # Trim leading and trailing quotes.
+#                         i = ( (i.is_a? Integer )  ? Integer(i) : # Convert to int or float if possible.
+#                            ((i.is_a? Float) ?   Float(i) : i))
+#                         })]
 
-    ## DEBUG: $stderr.puts "#{recnum += 1} / #{rec['Date']} / #{rec['Name']} / #{rec['Location']}"
+#     ## DEBUG: $stderr.puts "#{recnum += 1} / #{rec['Date']} / #{rec['Name']} / #{rec['Location']}"
 
-    # Add a "Singers" field to the record by parsing the Minutes
-    # field.  It will be a list of records of singers with 'name' and
-    # 'songs', which itself will be a list of song markups.
+#     # Add a "Singers" field to the record by parsing the Minutes
+#     # field.  It will be a list of records of singers with 'name' and
+#     # 'songs', which itself will be a list of song markups.
 
-    minutes     = rec["Minutes"]
+#     minutes     = rec["Minutes"]
 
-    singers_raw   = minutes.scan(singer_match)
+#     singers_raw   = minutes.scan(singer_match)
 
-    rec['Singers']  = singers_raw.map{|pair|
-      name, songs   = pair
-      songlist    = songs.scan(song_markup).flatten
-      info      = {
-      'name'    => name,
-      # 'songs_raw' => songs,
-      'songs'   => songlist,
-      }
-    }
+#     rec['Singers']  = singers_raw.map{|pair|
+#       name, songs   = pair
+#       songlist    = songs.scan(song_markup).flatten
+#       info      = {
+#       'name'    => name,
+#       # 'songs_raw' => songs,
+#       'songs'   => songlist,
+#       }
+#     }
 
 
-  end
-  rec
-end
+#   end
+#   rec
+# end
 
 class SingingsController < OpenReadController
+  include MinutesParser
+  include DensonParseOne
+
   def index
     if params[:name]
         singings_by_name = Array.new
--- lib/minutes_parser.rb
+++ lib/minutes_parser.rb
@@ -1,28 +1,74 @@
-# require 'csv'
-# require_relative 'denson_parse_one.rb'
+require 'csv'
+require './denson_parse_one.rb'
 
-# def parse_minutes_shenandoah singing_id, params
-#     # render json: singing_id
-#     book_id = Book.find_by(name:"Shenandoah Harmony")[:id]
-#     CSV.foreach(params[:csv], {headers: true}) do |call|
-#       song_id = Song.create_or_find_by!(number: call["Page"], name: call["Song Title"], book_id: book_id)
-#       caller_id = Caller.create_or_find_by!(name: call["Name(s)"])[:id]
-#       Call.create!(song_id: song_id, caller_id: caller_id, singing_id: singing_id)
-#     end
-# end
+def parse_minutes_shenandoah singing_id, csv
+    # render json: singing_id
+    calls = Array.new
+    book_id = Book.find_by(name:"Shenandoah Harmony")[:id]
 
-# def parse_minutes_denson singing_id, params
-#   # book_id = Book.find_by(name:"1991 Sacred Harp")[:id]
-#   # minutes = denson_parse_one(params[:csv])
-#   # minutes["Singers"].each do |call|
-#   #   caller_id = Caller.create_or_find_by!(name: call["name"])[:id]
-#   #   call["songs"].each do |song|
-#   #     song_id = Song.find_by(number: song, book_id: book_id)[:id]
-#   #     Call.create!(song_id: song_id, caller_id: caller_id, singing_id: singing_id)
-#   #   end
-#   # end
-# end
+    CSV.parse(csv, {headers: true}) do |call|
 
+        song_id = Song.find_or_create_by!(number: call["Page"], name: call["Song Title"], book_id: book_id)[:id]
+        caller_id = Caller.find_or_create_by!(name: call["Name(s)"])[:id]
+
+        new_call = Call.create!(song_id: song_id, caller_id: caller_id, singing_id: singing_id)
+        calls.push(new_call)
+    
+        
+    end
+    render json: {calls: calls}
+end
+
+def parse_minutes_denson singing_id, csv
+  puts "singing id = ", singing_id
+  book_id = Book.find_by(name:"1991 Sacred Harp")[:id]
+  minutes = denson_parse_one(csv)
+  minutes["Singers"].each do |call|
+  def create_call_for_existing_song song_number, caller_id, singing_id
+    found = Song.find_by!(number: song_number)
+    song_id = found[:id]
+    Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song_id)
+  end
+
+  caller_id = Caller.find_or_create_by!(name: call["name"])[:id]
+  call["songs"].each do |song|
+    # matches unambiguous calls
+      # returns MatchData or nil
+      # capture 1 is song number
+      match =  /\[(\d+[tb]?)\]/.match(song)
+      if match 
+         create_call_for_existing_song match[1], caller_id, singing_id
+      else
+      # matches ambiguous calls
+      # returns MatchData or nil
+      # capture 1 is song number
+        match = /\{(\d+[tb]?)\}/.match(song)
+        if match
+          song = Song.find_or_create_by!(number: match[1], book_id: book_id, name: "Ambiguous Call")
+          Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
+        else
+          # matches corrected calls
+          # returns MatchData or nil
+          # capture 1 is original song number
+          # capture 2 is corrected song number
+          match = /\[(\d+[tb]?)\/\/(\d+[tb]?)\]/.match(song)
+          if match
+            create_call_for_existing_song match[2], caller_id, singing_id
+          else
+            # matches uncorrectable calls
+            # returns MatchData or nil
+            # capture 1 is song number
+            match = /<(\d+[tb]?)>/.match(song)
+            if match
+              song = Song.find_or_create!(number: match[1], book_id: book_id, name: "Uncorrectable Call")
+              Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
+            end
+          end
+        end
+      end
+    end
+  end
+end
 
 
 
