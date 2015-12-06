d27681964f8b7979417a2cbbec1ef7aec5b9ea15
racheltstevens <racheltstevens@gmail.com>
2015-12-06 12:22:30 -0500

    Changes Shenandoah parser to reflect the csv you would export from their excel doc

--- lib/minutes_parser.rb
+++ lib/minutes_parser.rb
@@ -1,67 +1,68 @@
-require 'csv'
-require './denson_parse_one.rb'
 
-def parse_minutes_shenandoah singing_id, csv
-    # render json: singing_id
-    calls = Array.new
-    book_id = Book.find_by(name:"Shenandoah Harmony")[:id]
+module MinutesParser
+  require 'csv'
+  def parse_minutes_shenandoah singing_id, csv
+      # render json: singing_id
+      calls = Array.new
+      book_id = Book.find_by(name:"Shenandoah Harmony")[:id]
 
-    CSV.parse(csv, {headers: true}) do |call|
+      CSV.parse(csv, {headers: true}) do |call|
 
-        song_id = Song.find_or_create_by!(number: call["Page"], name: call["Song Title"], book_id: book_id)[:id]
-        caller_id = Caller.find_or_create_by!(name: call["Name(s)"])[:id]
+          song_id = Song.find_or_create_by!(number: call["Page"], name: call["Song Title - should appear when page is refreshed"], book_id: book_id)[:id]
+          caller_id = Caller.find_or_create_by!(name: call["Name(s)"])[:id]
 
-        new_call = Call.create!(song_id: song_id, caller_id: caller_id, singing_id: singing_id)
-        calls.push(new_call)
-    
-        
-    end
-    render json: {calls: calls}
-end
-
-def parse_minutes_denson singing_id, csv
-  puts "singing id = ", singing_id
-  book_id = Book.find_by(name:"1991 Sacred Harp")[:id]
-  minutes = denson_parse_one(csv)
-  minutes["Singers"].each do |call|
-  def create_call_for_existing_song song_number, caller_id, singing_id
-    found = Song.find_by!(number: song_number)
-    song_id = found[:id]
-    Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song_id)
+          new_call = Call.create!(song_id: song_id, caller_id: caller_id, singing_id: singing_id)
+          calls.push(new_call)
+      
+          
+      end
+      render json: {calls: calls}
   end
 
-  caller_id = Caller.find_or_create_by!(name: call["name"])[:id]
-  call["songs"].each do |song|
-    # matches unambiguous calls
-      # returns MatchData or nil
-      # capture 1 is song number
-      match =  /\[(\d+[tb]?)\]/.match(song)
-      if match 
-         create_call_for_existing_song match[1], caller_id, singing_id
-      else
-      # matches ambiguous calls
-      # returns MatchData or nil
-      # capture 1 is song number
-        match = /\{(\d+[tb]?)\}/.match(song)
-        if match
-          song = Song.find_or_create_by!(number: match[1], book_id: book_id, name: "Ambiguous Call")
-          Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
+  def parse_minutes_denson singing_id, csv
+    puts "singing id = ", singing_id
+    book_id = Book.find_by(name:"1991 Sacred Harp")[:id]
+    minutes = denson_parse_one(csv)
+    minutes["Singers"].each do |call|
+    def create_call_for_existing_song song_number, caller_id, singing_id
+      found = Song.find_by!(number: song_number)
+      song_id = found[:id]
+      Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song_id)
+    end
+
+    caller_id = Caller.find_or_create_by!(name: call["name"])[:id]
+    call["songs"].each do |song|
+      # matches unambiguous calls
+        # returns MatchData or nil
+        # capture 1 is song number
+        match =  /\[(\d+[tb]?)\]/.match(song)
+        if match 
+           create_call_for_existing_song match[1], caller_id, singing_id
         else
-          # matches corrected calls
-          # returns MatchData or nil
-          # capture 1 is original song number
-          # capture 2 is corrected song number
-          match = /\[(\d+[tb]?)\/\/(\d+[tb]?)\]/.match(song)
+        # matches ambiguous calls
+        # returns MatchData or nil
+        # capture 1 is song number
+          match = /\{(\d+[tb]?)\}/.match(song)
           if match
-            create_call_for_existing_song match[2], caller_id, singing_id
+            song = Song.find_or_create_by!(number: match[1], book_id: book_id, name: "Ambiguous Call")
+            Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
           else
-            # matches uncorrectable calls
+            # matches corrected calls
             # returns MatchData or nil
-            # capture 1 is song number
-            match = /<(\d+[tb]?)>/.match(song)
+            # capture 1 is original song number
+            # capture 2 is corrected song number
+            match = /\[(\d+[tb]?)\/\/(\d+[tb]?)\]/.match(song)
             if match
-              song = Song.find_or_create!(number: match[1], book_id: book_id, name: "Uncorrectable Call")
-              Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
+              create_call_for_existing_song match[2], caller_id, singing_id
+            else
+              # matches uncorrectable calls
+              # returns MatchData or nil
+              # capture 1 is song number
+              match = /<(\d+[tb]?)>/.match(song)
+              if match
+                song = Song.find_or_create!(number: match[1], book_id: book_id, name: "Uncorrectable Call")
+                Call.create!(singing_id: singing_id, caller_id: caller_id, song_id: song[:id])
+              end
             end
           end
         end
