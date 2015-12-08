1a0cd74368e4bd1d66a1e88a5ebb296bf9ebd267
racheltstevens <racheltstevens@gmail.com>
2015-12-08 18:44:09 -0500

    Adds book to search parameters for song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,14 @@
 class SongsController < OpenReadController
   def index
     if params[:number]
-      render json: Song.find_by(number: params[:number])
+      book_id = Book.find_by!(name: params[:book]).id
+      render json: Song.find_by!(number: params[:number], book_id: book_id )
     else
       render json: Song.all
     end
   end
 
   def show
-    render json: Song.find(params[:id])
+    render json: Song.where(params[:id])
   end
 end
