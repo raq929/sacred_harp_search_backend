956f8eb6aa3cc2b98f7b282bf95e89ad775b6ace
racheltstevens <racheltstevens@gmail.com>
2015-12-09 23:06:32 -0500

    Imports callInfo

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,13 +1,6 @@
 class CallsController < OpenReadController
-
-  def callInfo(call)
-     caller = Caller.find(call.caller_id)
-     singing = Singing.find(call.singing_id)
-     song = Song.find(call.song_id)
-
-     {book: song.book, song: {number: song.number, name: song.name}, singing: {name: singing.name, location: singing.location, date: singing.date, id: singing.id}, caller: caller.name}
-  end
-
+  include CallInfo
+ 
   def index
     #if its a request from a certain caller
     result = []
